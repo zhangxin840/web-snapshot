3ac79f63c258c2d49943b0c4ad8c2a1ef0f3315f
XinZhang <zhangxin19@meituan.com>
2016-03-19 00:22:39 +0800

    Use fs to output file.

--- snapshot.js
+++ snapshot.js
@@ -1,30 +1,38 @@
-var utils = require('./tools/utils')
+var fs = require('fs');
+var utils = require('./tools/utils');
 var shellRunner = require('./tools/shellRunner').setCommand('wkhtmltoimage');
-var imagesPath = 'public/snapshots/';
 
-var take = function (url) {
-    var options = {
-        output: imagesPath + utils.getMd5(url) + '.jpg',
+var getFilePath = function (url, options) {
+    var imagesPath = 'public/snapshots/';
+    var format = '.jpg';
+    return imagesPath + utils.getMd5(url) + format;
+};
+
+var take = function (url, options) {
+    var options = options || {
         quality: 100
     };
+    var filePath = getFilePath(url, options);
+    var childProcess;
 
-    console.log('run', url, options);
+    console.log('Start snapshot', url, options, filePath);
 
-    var wkhtmltoimage = shellRunner.run(url, options);
+    childProcess = shellRunner.run(url, options);
+    childProcess.stdout.pipe(fs.createWriteStream(filePath));
 
-    wkhtmltoimage.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+    // childProcess.stdout.on('data', (data) => {
+    //     console.log(`stdout: ${data}`);
+    // });
+
+    childProcess.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
     });
 
-    wkhtmltoimage.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
+    childProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
     });
 
-    wkhtmltoimage.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-    });
-
-    return child;
+    return childProcess;
 };
 
 module.exports = {
