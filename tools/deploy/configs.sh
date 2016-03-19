fa3f26378e8e8168dcb88b3c2477ba3996c2b84c
XinZhang <zhangxin19@meituan.com>
2016-03-19 14:36:22 +0800

    Add config for branch.

--- tools/deploy/configs.sh
+++ tools/deploy/configs.sh
@@ -1,5 +1,6 @@
 appName='web-snapshot'
 deployPath=/opt/$appName
 gitRepo=https://github.com/zhangxin840/web-snapshot.git
+releaseBranchName='master'
 installPath=~/tools
 binPath=/usr/local/bin
--- tools/deploy/updateCode.sh
+++ tools/deploy/updateCode.sh
@@ -3,5 +3,6 @@ source ./configs.sh
 [ -d $deployPath ] || git clone $gitRepo $deployPath
 
 git fetch origin
-git checkout -f master
-git reset --hard origin/master
+git checkout -f $releaseBranchName
+git reset --hard origin/$releaseBranchName
+git clean -dff # Remove untracked files
