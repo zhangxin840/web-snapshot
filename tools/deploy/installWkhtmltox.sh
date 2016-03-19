33ffd29ce44671959bdeddf90585c74c96cddcf6
XinZhang <zhangxin19@meituan.com>
2016-03-19 13:18:14 +0800

    Fix deploy scripts.

--- tools/deploy/installWkhtmltox.sh
+++ tools/deploy/installWkhtmltox.sh
@@ -5,13 +5,12 @@ echo 'Installing wkhtmltox'
 [ -d $installPath ] || mkdir -p $installPath
 cd $installPath
 
-wget -O wk.tar.xz 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz'
-sudo rm -rf wkhtmltox >/dev/null
-tar xvfJ wk.tar.xz $installPath
-sudo rm -rf wk.tar.xz
+wget -O $installPath/wk.tar.xz 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz'
+sudo rm -rf $installPath/wkhtmltox >/dev/null
+tar xvfJ $installPath/wk.tar.xz $installPath
+sudo rm -rf $installPath/wk.tar.xz
 
 export PATH=$PATH:$installPath/wkhtmltox/bin
-
 
 if which wkhtmltoimage >/dev/null; then
   echo 'Install wkhtmltox success'
--- tools/deploy/updateCode.sh
+++ tools/deploy/updateCode.sh
@@ -1,4 +1,7 @@
 source ./configs.sh
 
 [ -d $deployPath ] || git clone $gitRepo $deployPath
+
+git fetch origin
 git checkout -f master
+git reset --hard origin/master
