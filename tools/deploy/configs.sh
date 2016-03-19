32ce1ff8f6a974325678dc8704f04e994c812070
XinZhang <zhangxin19@meituan.com>
2016-03-19 14:22:25 +0800

    Fix wkhtmltoimage install script.

--- tools/deploy/configs.sh
+++ tools/deploy/configs.sh
@@ -1,4 +1,5 @@
-appName = 'web-snapshot'
-deployPath = /opt/$appName
-gitRepo = https://github.com/zhangxin840/web-snapshot.git
+appName='web-snapshot'
+deployPath=/opt/$appName
+gitRepo=https://github.com/zhangxin840/web-snapshot.git
 installPath=~/tools
+binPath=/usr/local/bin
